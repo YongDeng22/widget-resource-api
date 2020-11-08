f3a6ef2ead9c30111a5ee6fd574b6e090b728c83
Yong Deng <YongDeng22@users.noreply.github.com>
2020-11-08 16:30:43 -0500

    Added unit tests

--- tests/routes/widgetRoute.spec.ts
+++ tests/routes/widgetRoute.spec.ts
@@ -1,6 +1,36 @@
+import request from 'supertest';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import { widgetRoutes } from '../../src/routes/widgetRoute';
+import { widgetServices } from '../../src/services/widget.service';
+import { doesNotMatch } from 'assert';
+
+const fetchWidgetMock = jest.fn();
+widgetServices.fetchWidget = fetchWidgetMock;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/widget', widgetRoutes);
+
+jest.mock('../../src/services/widget.service');
+
+
+function fakeServiceResult() {
+    return {
+            id: 1,
+            name: 'test1',
+            type: 'type1'
+        }
+}
 
 describe('widget route', () => {
-    test ('test', () => {
-        expect(1).toEqual(1);
+    test ('should return result', async () => {
+        fetchWidgetMock.mockResolvedValueOnce(Promise.resolve(fakeServiceResult()));
+
+        const result = await request(app).get('/widget?id=1');
+        expect(result.status).toEqual(200);
+        expect(result).toEqual(fakeServiceResult());
     })
 })
